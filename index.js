764d85b65f6870112f9b8ab90a5d02e7e8ca3326
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-10-29 08:31:04 +0530

    added js for player active state

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
 const boxes = document.querySelectorAll('.box');
+const playerOne = document.getElementById('player-one');
+const playerTwo = document.getElementById('player-two');
 let playerOneTurn = true;
+
+if (playerOneTurn) {
+  playerOne.classList.add('active');
+  playerTwo.classList.remove('active');
+} else {
+  playerTwo.classList.add('active');
+  playerOne.classList.remove('active');
+}
+
+boxes.forEach(box => {
+  box.addEventListener('click', display);
+});
